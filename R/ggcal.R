3fa0b1c62e33dbc33c0af3f2eebbc930ad8e96df
ChopperCodes <website@meaningful-solutions.com>
2015-10-23 23:03:20 -0500

    print.days
    Added the print.days arguement in ggcal and updated package
    documentation.

--- R/ggcal.R
+++ R/ggcal.R
@@ -1,6 +1,6 @@
 require(ggplot2);
 ggcal<-
-  function(years, mo=NULL, q=NULL){
+  function(years, mo=NULL, q=NULL, print.days=FALSE){
     cal<-
       createCalendarTable(years=years, mo=mo, q=q);
     
@@ -10,10 +10,15 @@ ggcal<-
     gg<-
       (gg
        + geom_rect(aes(xmin=wday.nbr, xmax=wday.nbr+1, ymin=wk.nbr*-1, ymax=(wk.nbr*-1)+1), color="white")
-       + scale_x_continuous( label="", breaks=NULL)
-       + scale_y_continuous( label="", breaks=NULL)
+       + scale_x_continuous( name="", breaks=NULL)
+       + scale_y_continuous( name="", breaks=NULL)
        + facet_grid(monthf ~ year)
         );
+    
+    if(print.days){
+      gg<-
+        (gg + geom_text(aes(label=day, x=wday.nbr, y=(wk.nbr * -1)), color="white", size=3, vjust= -.75, hjust= -.35))
+    }
     
     return(gg);
   }
